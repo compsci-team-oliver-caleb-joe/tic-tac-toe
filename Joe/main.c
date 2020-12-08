9b95d9f897c307b7a80846ba8bf94731194d9096
joe <joe@joeth.uk>
2020-12-08 17:17:54 +0000

    add stuff

--- Joe/main.c
+++ Joe/main.c
@@ -9,14 +9,14 @@ int validChoice = 0;
 void chooseSpace(int columnChosen, int rowChosen, bool grid[3][3]){
     do{
         if(grid[columnChosen][rowChosen] == 1){
-            printf("You can't place in that square, it is already full");
+            printf("You can't place in that square, it is already full\n");
 
         } else if(grid[columnChosen][rowChosen] == 0){
             grid[columnChosen][rowChosen] = 1;
             validChoice = 1;
-            printf("square chosen");
+            printf("square chosen\n");
         } else{
-            printf("ERROR, invalid selection");
+            printf("ERROR, invalid selection\n");
         }
     }
     while(validChoice != 1);
@@ -28,6 +28,9 @@ int main() {
     int rowChosen = 0;
     int turnNum = 0;
 
-
+    printf("Welcome to naughts and crosses\n");
+    printf("First player, choose where you place your cross");
+    scanf("%d", &rowChosen);
+    printf("");
     return 0;
 }
